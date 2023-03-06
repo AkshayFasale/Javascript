44e428783dfa072427e7592cfcd1e01bf6afd51b
Akshay Fasale <akshayfasale@Akshays-Air.STELFIBER>
2023-03-06 08:47:53 +0530

    second question

---
+++ SumOfEvenNumTillN.js
@@ -0,0 +1,16 @@
+// Sum of Even Numbers till N -JS
+// Given a number N, print sum of all even numbers from 1 to N.
+
+var N = 6;
+
+// let N = parseInt(readLine());
+let i;
+let sum = 0;
+
+for (i = 1; i <= N; i++) {
+    if (i % 2 == 0) {
+        sum = sum + i;
+    }
+}
+
+console.log(sum);
