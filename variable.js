136f4dea7aa02570cab05478c87e12542c56171f
Akshay Fasale <akshayfasale@Akshays-MacBook-Air.local>
2023-03-06 22:45:02 +0530

    question solved

--- variable.js
+++ variable.js
@@ -1,12 +1,14 @@
 //Q.1 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
 
 
-const myName = "Akshay";
-const isWorking = true;
-const isPerfectInFrontend = false;
-const willHappyInSameJob = undefined;
+const myName = "Akshay"; //String type variable
+const myNum = 1; //number tyoe variale
+const isWorking = true; //boolean type variable
+const isPerfectInFrontend = null; //null type variable
+const willHappyInSameJob = undefined; //undefined type variable
 
 console.log("The data type of " + myName + " is " + typeof myName);
+console.log("The data type of " + myNum + " is " + typeof myNum);
 console.log("The data type of " + isWorking + " is " + typeof isWorking);
 console.log("The data type of " + isPerfectInFrontend + " is " + typeof isPerfectInFrontend);
 console.log("The data type of " + willHappyInSameJob + " is " + typeof willHappyInSameJob);
