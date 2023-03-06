986dc3634efd3099f64fdd047e9f9bf6468f396f
Akshay Fasale <akshayfasale@Akshays-MacBook-Air.local>
2023-03-06 22:52:41 +0530

    question solved

---
+++ intro.js
@@ -0,0 +1,11 @@
+//02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+
+const firstName = "Akshay";
+const lastName = "Fasale";
+let maritalStatus = "UnMarried";
+const country = "India";
+let age = 23;
+
+const intro = `Hello! My name is ${firstName} ${lastName} and I am ${age} years old. I live in ${country} and currently ${maritalStatus}`;
+console.log(intro);
