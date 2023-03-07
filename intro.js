c555f64a227b997429a208177ad6e0d7058b200a
Akshay Fasale <akshayfasale@Akshays-Air.STELFIBER>
2023-03-07 21:47:12 +0530

    question solved

--- intro.js
+++ intro.js
@@ -7,5 +7,9 @@ let maritalStatus = "UnMarried";
 const country = "India";
 let age = 23;
 
+// Interpolate method
 const intro = `Hello! My name is ${firstName} ${lastName} and I am ${age} years old. I live in ${country} and currently ${maritalStatus}`;
 console.log(intro);
+
+// Traditional method
+console.log("Hello My name is " + firstName +" "+lastName + " I live in " +" "+ country + "and i am currently " + maritalStatus);
