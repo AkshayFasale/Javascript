84c065a7e4fe6d88b65be95fbe4e2f9749049fd6
Akshay Fasale <akshayfasale@Akshays-Air.STELFIBER>
2023-03-08 11:00:33 +0530

    question solved

---
+++ question05.js
@@ -0,0 +1,11 @@
+
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+let str = "Akshay Fasale";
+let arr = str.split(" ");
+
+console.log(arr);
+
+// other way without storing in another variable
+let str1 = "Fasale Akshay";
+console.log(str1.split(" "));
