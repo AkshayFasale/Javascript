aae1b521d0e6981f6f1d3d5bb3789066a85f9c8b
Akshay Fasale <fasaleakshay25@gmail.com>
2023-03-09 22:19:00 +0530

    question solved

---
+++ question06.js
@@ -0,0 +1,6 @@
+//06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+const strng = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+const arry = strng.split(",");
+
+console.log(arry);
