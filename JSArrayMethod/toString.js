c681eaa95e0993cbd1f6d8952553fb943c83354b
Akshay Fasale <fasaleakshay25@gmail.com>
2023-03-20 21:59:47 +0530

    Array method

---
+++ JSArrayMethod/toString.js
@@ -0,0 +1,8 @@
+// toString() return a string with array values separated by commas. And it does not change the original array.
+
+// syntaxt - > array.toString() 
+
+let bikes = ["Duke 250", "pulsar 220F", "Meteor 350", "FZ"];
+console.log(bikes.toString());
+
+// "Duke 250,pulsar 220F,Meteor 350,FS"
