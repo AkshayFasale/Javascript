a1267fb82c03ff9a161cbfd68f2bd1a97b06035a
Akshay Fasale <fasaleakshay25@gmail.com>
2023-06-01 13:46:04 +0530

    Question solved

---
+++ JSFunction/question02/script.js
@@ -0,0 +1,16 @@
+// Declare a function addNumbers and it takes two two parameters and it returns sum.
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    console.log(sum)
+}
+addNumbers(20, 30); // 50
+
+//Another way
+function addNumber() {
+    let first = 10;
+    let second = 20;
+    let output = first + second;
+    console.log(output)
+}
+addNumber(); // 50
