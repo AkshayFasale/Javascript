0bc035526b6740230a06635b15c24c20b0a7b4ce
Akshay Fasale <fasaleakshay25@gmail.com>
2023-06-01 19:55:08 +0530

    Question solved

---
+++ JSFunction/Question03/script.js
@@ -0,0 +1,10 @@
+// An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+
+function areaOfRectangle(length, width) {
+    let area = length * width;
+
+    console.log(`Area of rectangle is ${area}`);
+}
+
+areaOfRectangle(10, 20); // 200
+areaOfRectangle(4,9) // 36
