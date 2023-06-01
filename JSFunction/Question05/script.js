6c25f8d0f93b722fc8e93fc5136a699b080968f1
Akshay Fasale <fasaleakshay25@gmail.com>
2023-06-01 22:21:54 +0530

    Question solved

---
+++ JSFunction/Question05/script.js
@@ -0,0 +1,11 @@
+// A volume of a rectangular prism is calculated as
+// follows: volume = length x width x height.Write a function which calculates volumeOfRectPrism.
+
+
+
+function volumeOfRectPrism(length, width, height) {
+    let volume = length * width * height;
+
+    console.log(`Volume of rectangular prism is ${volume}`);
+}
+volumeOfRectPrism(10, 5, 20);  //1000
