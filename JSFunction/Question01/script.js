49b2b137b37e4a784bae4fadafe3103e13f5df65
Akshay Fasale <fasaleakshay25@gmail.com>
2023-06-01 13:41:44 +0530

    Question solved

---
+++ JSFunction/Question01/script.js
@@ -0,0 +1,8 @@
+// Declare a function fullName and it print out your full name.
+
+function fullName() {
+    let FirstName = "Akshay";
+    let LastName = "Fasale";
+    console.log(FirstName + " " + LastName);
+}
+fullName(); // Akshay Fasale
