40ffee1aa69ca5443e68510fd1be9d50b5bf4e05
Akshay Fasale <fasaleakshay25@gmail.com>
2023-06-02 20:24:56 +0530

    Solved using Arrow Function

--- JSFunction/Question06/script.js
+++ JSFunction/Question06/script.js
@@ -11,3 +11,10 @@ areaOfCircle(3.2); // Area of square is 32.17
 
 areaOfCircle(3) // Area of square is 28.27
 
+const circle = (r) => {
+    const PII = Math.PI;
+    let circleArea = PII * r * r;
+
+    console.log(`Area of circle is ${circleArea.toFixed(2)}`);
+}
+circle(4.2) // Area of circle is 55.42
